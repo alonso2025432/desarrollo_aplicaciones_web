601a40fd78591795775b5e0cd3fa0f156ada6044
alonso2025432 <alonsozuniga@liceovvh.cl>
2025-07-08 09:26:33 -0400

    o

--- js/05_Condicionales_if_con_Arreglos_y_Métodos/01/static/js/script.js
+++ js/05_Condicionales_if_con_Arreglos_y_Métodos/01/static/js/script.js
@@ -12,7 +12,7 @@ function mayoriaedad(){
     }
 }
 
-function mostrarNuevoProducto(){
+function mostrarnuevoproducto(){
     let productos = ["pan", "leche", "queso"];
     let nuevoProducto = prompt("Ingrese un nuevo Producto");
     if (productos[0] == nuevoProducto){
@@ -30,4 +30,41 @@ function mostrarNuevoProducto(){
         `<p>Nueva lista de productos: <strong>${productos.join(" - ")}</strong> ingresado.</p>`;
 }
 
+}
+let tareas = ["matematica","lenguaje","filosofia","POO","ciencias","historia"]
+function controldetareas(){
+    
+    if (tareas.length > 5){
+        tareas.pop()
+        document.getElementById("resultado").innerHTML =
+        `<p>listado de tareas: ${tareas}</p>`;
+    } else {
+        document.getElementById("resultado").innerHTML =
+        `<p>lista aceptable</p>`;
+    }
+}
+let cola = ["SinNombre","pedro","lucia"]
+function validarprimerelemento(){
+    
+    if (cola[0]=="SinNombre"){
+        cola.shift()
+        document.getElementById("resultado").innerHTML = 
+        `<p>cola: ${cola}</p>`;
+    }else {
+        document.getElementById("resultado").innerHTML =
+        `<p>cola correcta</p>`;
+    }
+}
+
+function saludocondicional(){
+    let nombre = ("alonso")
+    if(nombre.length == 0){
+        document.getElementById("resultado").innerHTML =
+        `<p>nombre no valido</p>`
+    }else {
+        let saludos = ["hola "]
+        saludos.push(nombre)
+        document.getElementById("resultado").innerHTML =
+        `<p>${saludos}</p>`
+    }
 }
