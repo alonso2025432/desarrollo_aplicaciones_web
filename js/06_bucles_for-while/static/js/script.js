7c11c614e81c782d4638abefdbe59646062ab1b4
alonso2025432 <alonsozuniga@liceovvh.cl>
2025-07-11 12:56:07 -0400

    i

--- js/06_bucles_for-while/static/js/script.js
+++ js/06_bucles_for-while/static/js/script.js
@@ -23,3 +23,64 @@ function sumarNumeros(){
     document.getElementById("resultado2").innerHTML =
     `<div class="resultado"><p>La suma de: ${arr.join(" + ")} es: <strong>${suma}</strong></p></div>`;
 }
+
+numerosPares = [];
+function pares(){
+    numerosPares = [] // Reinicia el array
+    for (let i = 1; i <= 20; i++){
+        if (i % 2 === 0){
+            numerosPares.push(i)
+        }
+    }
+    document.getElementById("resultado3").innerHTML =
+    `<div class="resultado"><p>Número pares del 1 al 20: <strong>{${numerosPares.join(", ")}}</strong></p></div>`;
+}
+
+let lista = []
+function contadorRegresivo() {
+    let numero = parseInt(prompt("Ingrese un numero para la cuenta regresiva: "));
+    if (numero === 0) {
+        alert("Numero no valido");
+    }
+    while (numero >= 0) {
+        // alert(numero);
+        lista.push(numero);
+        numero--;
+    };
+    document.getElementById("resultado4").innerHTML = `<p>${lista.join("- ")}</p>`;
+
+};
+
+function mostrarTablaMultiplicar(){
+    let resultado = "";
+    let numero = parseInt(prompt("Ingrese número para trabajar: "));
+    for(let i = 1; i <= 10; i++){
+        let mult = numero * i;
+        resultado += `<p>${numero} x ${i} = ${mult}</p>`;
+    }
+    document.getElementById("resultado5").innerHTML = resultado;
+}
+
+let sumaAcumulada = [];
+function sumarHastaCero(){
+    let suma = 0;
+    let numero;
+    sumaAcumulada = [];
+    document.getElementById("resultado6").innerHTML = "";
+    while (true) {
+        numero = parseFloat(prompt("Introduce un número para sumar (0 para terminar)"));
+        if (isNaN(numero)){
+            document.getElementById(resultado6).innerHTML =
+            "<p>Por favor, introduce un número valido.</p>"
+            return
+        }
+        if (numero === 0){
+            break
+        }
+        suma += numero
+        sumaAcumulada.push(numero)
+    }
+    document.getElementById("resultado6").innerHTML =
+    `<p>Números sumados: <strong>${sumaAcumulada.join(", ")}
+    </strong><br>Total acumulado: <strong>${suma}</strong></p>`
+}
