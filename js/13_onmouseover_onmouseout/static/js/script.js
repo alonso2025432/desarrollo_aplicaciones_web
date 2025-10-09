b5b1dc612b02acfeb45dddf996d09362b203a8d7
Ignacio Alfredo <garridoignacio022@gmail.com>
2025-10-09 09:31:35 -0300

    fg

--- js/13_onmouseover_onmouseout/static/js/script.js
+++ js/13_onmouseover_onmouseout/static/js/script.js
@@ -1,3 +1,22 @@
-function play(elemento) {
-    
+function playvideo(element) {
+    element.play();
+    element.currentTime = 0;
+}
+function pausevideo(elemento){
+    elemento.pause();
+    elemento.currentTime = 0;
+}
+
+function cambioVideo(elemeto){
+    const videoGrande = document.getElementById("mainVideo");
+    let sourceVideoGrande = videoGrande.src;
+    videoGrande.src = elemeto.src;
+    elemeto.src = sourceVideoGrande;
+
+    const textoGrande = document.getElementById("texto");
+    let textoGrandeContent = textoGrande.textContent;
+    const textoPequeño = elemeto.nextElementSibling;
+    let textoPequeñoContent = textoPequeño.textContent;
+    textoGrande.textContent = textoPequeñoContent;
+    textoPequeño.textContent = textoGrandeContent
 }
