479d134251430a8fe7ac24912e7a514de37bb90a
Ignacio Alfredo <garridoignacio022@gmail.com>
2025-10-16 09:33:12 -0300

    gvb

--- js/14_Tiempo_onchange/static/js/script.js
+++ js/14_Tiempo_onchange/static/js/script.js
@@ -1,8 +1,45 @@
 function hide(elemento){
     elemento.remove();
 }
-function cambiar(elemento){
-    if (elemento === "°C"){
+function cambiar(){
+    const medida = document.getElementById("select").value;
+    const grados = Number(document.getElementById("grados").textContent);
+    const grados1 = Number(document.getElementById("grados1").textContent);
+    const grados2 = Number(document.getElementById("grados2").textContent);
+    const grados3 = Number(document.getElementById("grados3").textContent);
+    const grados4 = Number(document.getElementById("grados4").textContent);
+    const grados5 = Number(document.getElementById("grados5").textContent);
+    const grados6 = Number(document.getElementById("grados6").textContent);
+    const grados7 = Number(document.getElementById("grados7").textContent);
+    
+    const gradosf = (grados *1.8)+32;
+    const gradosf1 = (grados1 *1.8)+32;
+    const gradosf2 = (grados2 *1.8)+32;
+    const gradosf3 = (grados3 *1.8)+32;
+    const gradosf4 = (grados4 *1.8)+32;
+    const gradosf5 = (grados5 *1.8)+32;
+    const gradosf6 = (grados6 *1.8)+32;
+    const gradosf7 = (grados7 *1.8)+32;
+    
+    if (medida === "C"){
+        document.getElementById("grados").textContent = grados
+        document.getElementById("grados1").textContent = grados1
+        document.getElementById("grados2").textContent = grados2
+        document.getElementById("grados3").textContent = grados3
+        document.getElementById("grados4").textContent = grados4
+        document.getElementById("grados5").textContent = grados5
+        document.getElementById("grados6").textContent = grados6
+        document.getElementById("grados7").textContent = grados7
+    }else if (medida === "F"){
         
+
+        document.getElementById("grados").textContent = gradosf
+        document.getElementById("grados1").textContent = gradosf1
+        document.getElementById("grados2").textContent = gradosf2
+        document.getElementById("grados3").textContent = gradosf3
+        document.getElementById("grados4").textContent = gradosf4
+        document.getElementById("grados5").textContent = gradosf5
+        document.getElementById("grados6").textContent = gradosf6
+        document.getElementById("grados7").textContent = gradosf7
     }
 }
